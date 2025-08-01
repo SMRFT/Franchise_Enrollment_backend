311a3844531ec66de48010c33a72f8ed74057a15
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-08-01 16:28:40 +0530

    Update settings.py

--- franchise_backend/settings.py
+++ franchise_backend/settings.py
@@ -139,9 +139,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'resultssmrft@gmail.com'
+EMAIL_HOST_PASSWORD = 'nfkk krtt htoq asge'  # Consider using env vars for security
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
