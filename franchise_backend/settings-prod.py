722492ecd893dde29e383a4a09a38ac4f159f59c
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-08-01 16:30:48 +0530

    Update settings-prod.py

--- franchise_backend/settings-prod.py
+++ franchise_backend/settings-prod.py
@@ -81,10 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'franchise',  
-        'ENFORCE_SCHEMA': False,
         'CLIENT': {
             'host': os.getenv('MONGO_URL'),
-            'authMechanism': 'SCRAM-SHA-1',
         }
     }
 }
